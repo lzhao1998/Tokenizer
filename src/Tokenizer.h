1b29cf6ca281db7c94e1710e6bfc6350245d3036
lai zhe hao <lzhao1998@gmail.com>
2018-07-22 18:55:51 +0800

    Added skeleton of tokenizer project in source file

---
+++ src/Tokenizer.h
@@ -0,0 +1,12 @@
+#ifndef Tokenizer_H
+#define Tokenizer_H
+
+#include "Common.h"
+#include "Token.h"
+
+Tokenizer *initTokenizer(char *stringToTokenize);
+Token *advanceToken(Tokenizer *tokenizer);
+Token *getToken(Tokenizer *tokenizer);
+Token *peepToken(Tokenizer *tokenizer);
+
+#endif // Tokenizer_H
